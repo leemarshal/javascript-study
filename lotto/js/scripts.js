9238bc3a33adf14f8dd8bcd15bba527289861b97
gwlee <leemarshal@naver.com>
2024-03-17 00:52:17 +0900

    refactor: simple compare function

--- lotto/js/scripts.js
+++ lotto/js/scripts.js
@@ -31,12 +31,5 @@ function generateLottoNumbuers() {
 }
 
 function compare(a, b) {
-    if(a < b) {
-        return -1;
-    }
-    if(a > b) {
-        return 1;
-    }
-
-    return 0;
+    return a - b;
 }
