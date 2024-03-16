42cbfffab661bc9caf252d673f87f09eb3a9a715
gwlee <leemarshal@naver.com>
2024-03-17 00:50:08 +0900

    feat: auto generate lotto numbers

---
+++ lotto/js/scripts.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const lottoNumbers = generateLottoNumbuers();
+    lottoNumbers.sort(compare);
+    const wrapper = document.querySelector('.lotto-wrapper');
+    wrapper.innerHTML = '';
+    lottoNumbers.forEach(number => {
+        const ball = document.createElement('div');
+        ball.className = 'lotto-ball';
+        ball.textContent = number;
+        wrapper.appendChild(ball);
+    });
+});
+
+function generateLottoNumbuers() {
+    var list = [];
+    var result = [];
+
+    for(var i = 1; i <= 45; i++) {
+        list.push(i);
+    }
+
+    var index;
+
+    for(i = 0; i < 6; i++) {
+        index = Math.floor(Math.random() * list.length);
+        result.push(list[index]);
+        list.splice(index, 1);
+    }
+
+    return result;
+}
+
+function compare(a, b) {
+    if(a < b) {
+        return -1;
+    }
+    if(a > b) {
+        return 1;
+    }
+
+    return 0;
+}
